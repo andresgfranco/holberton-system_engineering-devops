494e530e48cb45806629005b592782f3d375d0e9
andresgfranco <andresgfranco0@gmail.com>
2021-04-05 22:03:15 +0000

    Server updated

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -4,7 +4,7 @@ file {'~/.ssh/config':
   replace => 'yes',
   path    => '~/.ssh/config',
   content => 'Host *
-     HostName 34.75.232.120
+     HostName 34.74.115.197
      User ubuntu
      IdentityFile ~/.ssh/holberton',
   mode    => '7000',
