fb63005444c8c9a29d3425192fc68030b73b6722
andresgfranco <andresgfranco0@gmail.com>
2021-05-03 22:14:03 -0500

    Adding script number 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""script to export data in the CSV format"""
+
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+    import csv
+    url = "https://jsonplaceholder.typicode.com/"
+    user_id = argv[1]
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    tasks = requests.get(url + "todos/?userId={}".format(user_id)).json()
+
+    with open("{}.csv".format(user_id), "w") as csvfile:
+      writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+      for task in tasks:
+        writer.writerow([user_id, user["username"],
+                        task["completed"], task["title"]])
