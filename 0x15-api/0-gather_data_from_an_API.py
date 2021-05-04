36d040b6977689a75ce294312122a31643e079fe
andresgfranco <andresgfranco0@gmail.com>
2021-05-03 22:03:05 -0500

    Moved import modules within the if name condition

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,10 +1,11 @@
 #!usr/bin/python3
 """Python script that, using this REST API, for a given employee ID,
 returns information about his/her TO-DO list progress."""
-import requests
-from sys import argv
+
 
 if __name__ == "__main__":
+    import requests
+    from sys import argv
     url = "https://jsonplaceholder.typicode.com/"
     user_id = argv[1]
     user = requests.get(url + "users/{}".format(user_id)).json()
