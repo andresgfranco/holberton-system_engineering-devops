a587e5794e83d53b5ebfb409306bb572a8731882
andresgfranco <andresgfranco0@gmail.com>
2021-05-17 19:07:19 -0500

    Correction on last conditional

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -11,12 +11,15 @@ def top_ten(subreddit):
     param = {'limit': 10}
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     req = requests.get(
-        url, headers=header, allow_redirects=False, params=param
-        )
+        url,
+        headers=header,
+        allow_redirects=False,
+        params=param
+    )
 
     if req.status_code == 200:
         children = req.json().get('data').get('children')
         for i in children:
             print(i.get('data').get('title'))
     else:
-        return ('None')
+        print('None')
