ba3d5a688f321b90f3709772f0fac002bd318f31
andresgfranco <andresgfranco0@gmail.com>
2021-05-24 14:31:54 -0500

    Correction on redirection link

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -3,11 +3,11 @@ package { 'nginx':
   ensure => installed,
 }
 
-file_line { 'test':
+file_line { 'aaaaa':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=adLGHcj_fmA permanent;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
 file { '/var/www/html/index.html':
